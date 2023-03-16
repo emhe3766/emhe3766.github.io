c73c2c67af4e7ccdefe57c0ffbb601c21f921c8e
emhe3766 <emhe3766@colorado.edu>
2023-03-16 11:19:50 -0600

    update

--- JS/WA9.js
+++ JS/WA9.js
@@ -4,9 +4,9 @@ const customName = document.getElementById('customname');
 const story = document.querySelector('.story');
 
 let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+let insertX = ['Danny', 'Big Papa', 'Alice'];
+let insertY = ['the grocery store', 'the Hollywood sign', 'the largest ball of twine'];
+let insertZ = ['fell thought the ground', 'disintegrate into dust', 'turned into a frog'];
 
 function randomValueFromArray(array){
     const random = Math.floor(Math.random()*array.length);
@@ -23,7 +23,7 @@ function result() {
 
     if(customName.value !== '') {
         const name = customName.value;
-        newStory = newStory.replace('Bob', name);
+        newStory = newStory.replace('Nugget', name);
     }
     if(document.getElementById("uk").checked) {
         const weight = Math.round(300/14);
