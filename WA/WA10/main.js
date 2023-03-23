d5908665ed7555970239282d935a542aa2bfcab5
emhe3766 <emhe3766@colorado.edu>
2023-03-23 11:00:46 -0600

    update

---
+++ WA/WA10/main.js
@@ -0,0 +1,42 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const pics = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg']
+/* Declaring the alternative text for each image file */
+const alts = ['Closeup of a human eye', 'Marble', 'White and purple flowers', 'egyptian art', 'Butterfly']
+
+function setDisplay(p, a) {
+    displayedImage.setAttribute('src', p);
+    displayedImage.setAttribute('alt', a);
+}
+/* Looping through images */
+
+for (let i = 0; i < pics.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', pics[i]);
+    newImage.setAttribute('alt',  alts[i]);
+    newImage.addEventListener('click', () => setDisplay(pics[i], alts[i]));
+    thumbBar.appendChild(newImage);
+}
+
+// displayedImage. need to set src and alt text to match thumbnail
+/* Wiring up the Darken/Lighten button */
+btn.setAttribute("class", "dark");
+function change(){
+    if (btn.className == "dark"){
+        btn.className = "light"
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    }
+    else{
+        btn.className = "dark"
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+}
+btn.addEventListener('click', change);
+
