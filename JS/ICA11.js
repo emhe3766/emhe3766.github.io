0ca6b0991f181881cb6105bb9bf41fe7864085d5
emhe3766 <emhe3766@colorado.edu>
2023-04-04 12:07:46 -0600

    update

--- JS/ICA11.js
+++ JS/ICA11.js
@@ -1,23 +1,25 @@
 const button = document.querySelector("#js-new-quote");
 button.addEventListener('click', getQuote);
 
-const answerButton = document.querySelector("#js-text");
+const answerButton = document.querySelector("#js-tweet");
 answerButton.addEventListener('click', showAnswer);
 
 const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
-async function getQuote(){
+
+async function getQuote() {
     // console.log("It works!");
-    try{
-        const response = await fetch(endpoint);
+    try {
+        const response = await fetch(endpoint)
         if (!response.ok){
-            throw Error(response.statusText);
+            throw Error(response.statusText)
         }
         const json = await response.json();
-        // console.log(json.question);
+        console.log(json.question);
+        console.log(json.answer);
         displayQuote(json.question);
         showAnswer(json.answer);
     }
-    catch(err){
+    catch(err) {
         console.log(err);
         alert('Failed to fetch new trivia');
     }
