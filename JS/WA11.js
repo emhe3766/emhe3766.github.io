7898c1e2d4d64af7ede028e9bfaf787ef89cbea3
emhe3766 <emhe3766@colorado.edu>
2023-04-06 12:51:51 -0600

    update

--- JS/WA11.js
+++ JS/WA11.js
@@ -1,35 +1,50 @@
-const button = document.querySelector(".new-quote")
+const button = document.querySelector("#js-new-quote");
 button.addEventListener('click', getQuote);
 
-const answer = document.querySelector("#js-text");
-answerButton.addEventListener('click', showAnwser);
+const answerButton = document.querySelector("#js-tweet");
+answerButton.addEventListener('click', getPuppy);
 
-const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
-async function getQuote(){
+const endpoint = "https://www.boredapi.com/api/activity";
+const endpoint1 = "https://dog.ceo/api/breeds/image/random";
+
+async function getQuote() {
     // console.log("It works!");
-    try{
+    try {
         const response = await fetch(endpoint)
         if (!response.ok){
             throw Error(response.statusText)
         }
         const json = await response.json();
-        // console.log(json.question);
-        displayQuote(json.question);
-        showAnwser(json.answer);
+        console.log(json.activity);
+        displayQuote(json.activity);
     }
-    catch(err){
+    catch(err) {
         console.log(err);
         alert('Failed to fetch new trivia');
     }
 }
-
+async function getPuppy() {
+    // console.log("It works!");
+    try {
+        const response = await fetch(endpoint1)
+        if (!response.ok){
+            throw Error(response.statusText)
+        }
+        const json = await response.json();
+        document.getElementById("puppy").src = json.message;
+        console.log(json.message);
+    }
+    catch(err) {
+        console.log(err);
+        alert('Failed to fetch new trivia');
+    }
+}
 function displayQuote(quote){
-    const quoteText = document.querySelector("#js-quote-text")
+    const quoteText = document.querySelector("#js-quote-text");
     quoteText.textContent = quote;
 }
-function showAnwser(quote){
-    const answerText = document.querySelector("#js-answer-text")
+function showAnswer(quote){
+    const answerText = document.querySelector("#js-answer-text");
     answerText.textContent = quote;
 }
-
 getQuote();
