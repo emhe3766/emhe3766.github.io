69fe500d98bf147eb0c7937d0e558698d9686bc5
emhe3766 <emhe3766@colorado.edu>
2023-04-23 20:45:20 -0400

    update

--- JS/WA13.js
+++ JS/WA13.js
@@ -18,7 +18,6 @@ function minus() {
     output.textContent = outputInt; }
 
 }
-
 function plus() {
     if (outputInt < 100) {
     outputInt +=1;
@@ -26,46 +25,154 @@ function plus() {
     }
 }
 
-function random() {
+function Random() {
     outputInt = randomNumber(0, 100);
     output.textContent = outputInt;
 }
 
 function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
+    const Num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return Num;
   }
-
-
 
 const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
 console.log(outputInt);
 
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+// const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+// const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
 const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+const randomButton = document.querySelector('.random-button').addEventListener('click', Random);
 const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
 
 
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-button.addEventListener('click', updateOutput);
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
+//// for the moving + & - on the screen
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
 }
-*/
 
 
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
+// function to generate random color
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+class Ball{
+    constructor(x, y, xVel, yVel, color, size, pm) {
+        this.x = x;
+        this.y = y;
+        this.xVel = xVel;
+        this.yVel = yVel;
+        this.color = color;
+        this.size = size;
+        this.pm = pm;
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI, this.pm);
+        ctx.fill();
+    }
+    update() {
+      if ((this.x + this.size) >= width) {
+        this.xVel = -(this.xVel);
+      }
+
+      if ((this.x - this.size) <= 0) {
+        this.xVel = -(this.xVel);
+      }
+
+      if ((this.y + this.size) >= height) {
+        this.yVel = -(this.yVel);
+      }
+
+      if ((this.y - this.size) <= 0) {
+        this.yVel = -(this.yVel);
+      }
+
+      this.x += this.xVel;
+      this.y += this.yVel;
+    }
+    collisionDetect() {
+      for (const ball of balls) {
+        if (this !== ball) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+
+          if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+          }
+        }
+      }
+    }
+}
+
+// const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+function getMousePos(canvas, event) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top,
+  };
+}
+
+const balls = [];
+// how many balls we want
+while (balls.length < 25) {
+    const size = random(10,30);  // determining the smallest and largest ball we could have
+    const ball = new Ball(
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-5,5),
+        random(-5,5),
+        randomRGB(),
+        size
+    );
+
+    balls.push(ball);
+}
+// const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+function getXY(canvas, event){ //adjust mouse click to canvas coordinates
+  const rect = canvas.getBoundingClientRect()
+  const y = event.clientY - rect.top
+  const x = event.clientX - rect.left
+  return {x:x, y:y}
+}
+
+document.addEventListener("click",  function (plus) {
+  const XY = getXY(canvas, plus)
+  //use the shape data to determine if there is a collision
+  if(ctx.isPointInPath(path, XY.x, XY.y)) {
+    // Do Something with the click
+    plus()
+  }
+}, false)
+
+loop();
+
